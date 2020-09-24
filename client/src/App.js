73cbd4709fb7c37b8e085bff2bcd10ae60b90f65
yesha-mobio <yeshas.mobio@gmail.com>
2020-09-24 18:20:08 +0530

    Created Apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 // components
 import BookList from "./components/BookList";
 
+// Apollo Cient Set-Up
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_BASE_URI,
+});
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Ninja's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
