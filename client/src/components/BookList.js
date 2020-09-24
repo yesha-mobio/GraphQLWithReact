88f4a0ed6a81a10256441dae3c509643865c7a84
yesha-mobio <yeshas.mobio@gmail.com>
2020-09-24 22:18:42 +0530

    Add BookDetails component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 class BookList extends Component {
   displayBooks() {
@@ -20,6 +21,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails />
       </div>
     );
   }
